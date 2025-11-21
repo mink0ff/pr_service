73530bc13254cc997a22e6ddc401aa79ec88b4f0
mink0ff <minko.serezha@mail.ru>
2025-11-22 00:42:41 +0300

    реализовал сервисный слой для user

--- internal/dto/user.go
+++ internal/dto/user.go
@@ -1,10 +1,17 @@
 package dto
 
+import "github.com/google/uuid"
+
 type CreateUserRequest struct {
-	Name     string `json:"name" binding:"required"`
-	IsActive bool   `json:"isActive"`
+	Name     string    `json:"name" binding:"required"`
+	TeamID   uuid.UUID `json:"team_id" binding:"required"`
+	IsActive bool      `json:"isActive"`
 }
 
+type SetUserActiveRequest struct {
+	UserID   uuid.UUID `json:"user_id"`
+	IsActive bool      `json:"is_active"`
+}
 type UserResponse struct {
 	ID       int    `json:"id"`
 	Name     string `json:"name"`
--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -2,47 +2,57 @@ package service
 
 import (
 	"context"
+	"log"
 
+	"github.com/google/uuid"
 	"github.com/mink0ff/pr_service/internal/dto"
 	"github.com/mink0ff/pr_service/internal/models"
 	"github.com/mink0ff/pr_service/internal/repository"
 )
 
-type UserServiceImpl struct {
-	repo repository.UserRepository
+type UserService struct {
+	userRepo repository.UserRepository
 }
 
-func NewUserService(repo repository.UserRepository) *UserServiceImpl {
-	return &UserServiceImpl{repo: repo}
+func NewUserService(repo repository.UserRepository) *UserService {
+	return &UserService{userRepo: repo}
 }
 
-func (s *UserServiceImpl) Create(ctx context.Context, req *dto.CreateUserRequest) (*dto.UserResponse, error) {
-	id, err := s.repo.Create(ctx, models.User{
-		Name:     req.Name,
+func (s *UserService) CreateUser(ctx context.Context, req *dto.CreateUserRequest) (*models.User, error) {
+	userID := uuid.New()
+	user := models.User{
+		UserID:   userID,
+		Username: req.Name,
+		TeamID:   req.TeamID,
 		IsActive: req.IsActive,
-	})
+	}
+
+	err := s.userRepo.Create(ctx, user)
 
 	if err != nil {
+		log.Printf("user create error: %v", err)
+		return nil, ErrUserExists
+	}
+
+	return &user, nil
+}
+
+func (s *UserService) SetActive(ctx context.Context, req dto.SetUserActiveRequest) (*models.User, error) {
+	user, err := s.userRepo.GetByID(ctx, req.UserID)
+	if err != nil || user == nil {
+		log.Printf("user not found: %v", req.UserID)
+		return nil, ErrUserNotFound
+	}
+
+	user.IsActive = req.IsActive
+
+	if err := s.userRepo.Update(ctx, *user); err != nil {
 		return nil, err
 	}
 
-	return &dto.UserResponse{
-		ID:       id,
-		Name:     req.Name,
-		IsActive: req.IsActive,
-	}, nil
+	return user, nil
 }
 
-func (s *UserServiceImpl) GetUser(ctx context.Context, id int) (*dto.UserResponse, error) {
-	user, err := s.repo.GetByID(ctx, id)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return &dto.UserResponse{
-		ID:       user.ID,
-		Name:     user.Name,
-		IsActive: user.IsActive,
-	}, nil
+func (s *UserService) GetReviewPRs(ctx context.Context, userID uuid.UUID) ([]models.PullRequest, error) {
+	return s.userRepo.ListReviewPRs(ctx, userID)
 }
