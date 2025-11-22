418247da30f1428e55cff794a4dd57236b9d5283
mink0ff <minko.serezha@mail.ru>
2025-11-22 15:30:58 +0300

    новые модели

--- internal/models/pull_request.go
+++ internal/models/pull_request.go
@@ -2,8 +2,6 @@ package models
 
 import (
 	"time"
-
-	"github.com/google/uuid"
 )
 
 type PRStatus string
@@ -14,9 +12,9 @@ const (
 )
 
 type PullRequest struct {
-	PullRequestID   uuid.UUID  `db:"pull_request_id"`
+	PullRequestID   string     `db:"pull_request_id"`
 	PullRequestName string     `db:"pull_request_name"`
-	AuthorID        uuid.UUID  `db:"author_id"`
+	AuthorID        string     `db:"author_id"`
 	Status          PRStatus   `db:"status"`
 	CreatedAt       time.Time  `db:"created_at"`
 	MergedAt        *time.Time `db:"merged_at"`
--- internal/models/user.go
+++ internal/models/user.go
@@ -3,7 +3,7 @@ package models
 import "github.com/google/uuid"
 
 type User struct {
-	UserID   uuid.UUID `db:"user_id"`
+	UserID   string    `db:"user_id"`
 	Username string    `db:"username"`
 	TeamID   uuid.UUID `db:"team_id"`
 	IsActive bool      `db:"is_active"`
